d222d8c4a4af5ed70f25e89a167ae1c42c2075b6
yusufcanbircan <yusufcannnz@hotmail.com>
2023-01-12 21:13:00 +0300

    day6 completed

--- Days/Day1-15 - Basics of Swift Language.playground/Contents.swift
+++ Days/Day1-15 - Basics of Swift Language.playground/Contents.swift
@@ -400,3 +400,75 @@ let theme = Theme.dark
 
 let background = theme == .dark ? "black" : "white"
 print(background)
+
+
+// MARK: - Day 6 – loops, summary
+
+//// How to use a for loop to repeat work
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift works great on \(os).")
+}
+
+for i in 1...12 {
+    print("5 x \(i) is \(5 * i)")
+}
+
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+print()
+
+for i in 1..<5 {
+    print("Counting 1 up to 5: \(i)")
+}
+
+//// How to use a while loop to repeat work
+
+// create an integer to store our roll
+var roll = 0
+
+// carry on looping until we reach 20
+while roll != 20 {
+    // roll a new dice and print what it was
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+// if we're here it means the loop ended – we got a 20!
+print("Critical hit!")
+
+
+//// How to skip loop items with break and continue
+
+let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+
+    print("Found picture: \(filename)")
+}
+
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
+
+
