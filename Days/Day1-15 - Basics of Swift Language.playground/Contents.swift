40d286350c23a53e695dca67dd977bc49403e15f
yusufcanbircan <yusufcannnz@hotmail.com>
2023-01-14 23:00:04 +0300

    day8 completed

--- Days/Day1-15 - Basics of Swift Language.playground/Contents.swift
+++ Days/Day1-15 - Basics of Swift Language.playground/Contents.swift
@@ -547,3 +547,59 @@ let lastName = user.lastName
 print("Name: \(firstName) \(lastName)")
 
 
+// MARK: - Day 8 – default values, throwing functions
+
+//// How to provide default values for parameters
+
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5, end: 20)
+printTimesTables(for: 8)
+
+//// How to handle errors in functions
+
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+let string = "12345"
+
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch {
+    print("There was an error.")
+}
+
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
+}
