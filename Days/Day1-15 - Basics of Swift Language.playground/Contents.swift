34f7b1baf3ba29e08d7a1b848fb3ae9b549cdbc6
yusufcanbircan <yusufcannnz@hotmail.com>
2023-01-16 19:00:47 +0300

    day10 completed

--- Days/Day1-15 - Basics of Swift Language.playground/Contents.swift
+++ Days/Day1-15 - Basics of Swift Language.playground/Contents.swift
@@ -683,3 +683,119 @@ let uppercaseTeam = team.map { $0.uppercased() }
 print(uppercaseTeam)
 
 
+// MARK: - Day 10 – structs, computed properties, and property observers
+
+//// How to create your own structs
+
+struct Album { // simple struct
+    let title: String
+    let artist: String
+    let year: Int
+
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+//// How to compute property values dynamically
+
+struct Employee {
+    let name: String
+    var vacationRemaining: Int
+}
+
+var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+archer.vacationRemaining -= 5
+print(archer.vacationRemaining)
+archer.vacationRemaining -= 3
+print(archer.vacationRemaining)
+
+struct Employee {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+
+    var vacationRemaining: Int {
+        vacationAllocated - vacationTaken
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+archer.vacationTaken += 4
+print(archer.vacationRemaining)
+archer.vacationTaken += 4
+print(archer.vacationRemaining)
+
+var vacationRemaining: Int {
+    get {
+        vacationAllocated - vacationTaken
+    }
+
+    set {
+        vacationAllocated = vacationTaken + newValue
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+archer.vacationTaken += 4
+archer.vacationRemaining = 5
+print(archer.vacationAllocated)
+
+//// How to take action when a property changes
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+
+//// How to create custom initializers
+
+struct Player {
+    let name: String
+    let number: Int
+
+    init(name: String, number: Int) {
+        self.name = name
+        self.number = number
+    }
+}
+
+
