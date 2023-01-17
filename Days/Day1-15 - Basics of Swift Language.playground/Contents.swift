016a878214cf24277e954dfb2e9340d5a80d03e7
yusufcanbircan <yusufcannnz@hotmail.com>
2023-01-17 12:59:53 +0300

    day11 completed

--- Days/Day1-15 - Basics of Swift Language.playground/Contents.swift
+++ Days/Day1-15 - Basics of Swift Language.playground/Contents.swift
@@ -799,3 +799,65 @@ struct Player {
 }
 
 
+// MARK: - Day 11 – access control, static properties and methods
+
+//// How to limit access to internal data using access control
+
+struct BankAccount {
+    var funds = 0
+
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+}
+
+account.funds -= 1000 // we can change it directly, if we define funds as private, it is better.
+
+
+//// Static properties and methods
+
+
+struct School {
+    static var studentCount = 0
+
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+
+School.add(student: "Taylor Swift")
+print(School.studentCount)
+
+struct Employee {
+    let username: String
+    let password: String
+
+    static let example = Employee(username: "cfederighi", password: "hairforceone")
+}
+
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "settings.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+
