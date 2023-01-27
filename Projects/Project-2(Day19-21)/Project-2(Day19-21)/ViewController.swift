cb3ed6b0f4a24cb902849442ff561dc6e744a076
yusufcanbircan <yusufcannnz@hotmail.com>
2023-01-27 20:47:54 +0300

    day 19 comleted

---
+++ Projects/Project-2(Day19-21)/Project-2(Day19-21)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-2(Day19-21)
+//
+//  Created by Yusuf Can Bircan on 27.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
