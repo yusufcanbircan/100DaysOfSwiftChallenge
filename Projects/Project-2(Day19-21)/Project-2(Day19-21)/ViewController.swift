214d5cfeb9ee11ba62cedfd0edc904be66c31cb2
yusufcanbircan <yusufcannnz@hotmail.com>
2023-02-22 19:50:43 +0300

    layouts designed

--- Projects/Project-2(Day19-21)/Project-2(Day19-21)/ViewController.swift
+++ Projects/Project-2(Day19-21)/Project-2(Day19-21)/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
