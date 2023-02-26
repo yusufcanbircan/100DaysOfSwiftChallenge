b1a4e720c6719d5f04b6cb84f39f36ab38febb4e
yusufcanbircan <yusufcannnz@hotmail.com>
2023-02-26 19:35:56 +0300

    navigationItem added

--- Projects/Project-5(Day27-29)/Project-5(Day27-29)/ViewController.swift
+++ Projects/Project-5(Day27-29)/Project-5(Day27-29)/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsUrl) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -44,6 +46,26 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = usedWords[indexPath.row]
 
         return cell
+    }
+    
+    @objc func promptForAnswer() {
+        
+        let ac = UIAlertController(title: "Enter Answer..", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        
     }
 
 
